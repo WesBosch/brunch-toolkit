6837693a175f9f9039385b3a6de7d70372903d56
Wisteria <63926174+WesBosch@users.noreply.github.com>
2021-08-24 03:19:21 -0400

    Update update_patch.sh
    Add some error messages if things fail somewhere

--- update_patch.sh
+++ update_patch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo mkdir -p /var/brunch/toolkit
-sudo chown -R $USER /var/brunch
+sudo mkdir -p /var/brunch/toolkit || echo "Failed to create workspace."
+sudo chown -R $USER /var/brunch || echo "Failed to take ownership of workspace" 
 current_brunch_version=$(cat /etc/brunch_version 2>/dev/null |  cut -d' ' -f3 )
 
 if (( $current_brunch_version < 20210822 )) ; then
 echo "Downloading patched update script, please wait..."
-curl -l https://raw.githubusercontent.com/WesBosch/brunch-toolkit/main/chromeos-update -o /var/brunch/toolkit/chromeos-update
-sudo cp /var/brunch/toolkit/chromeos-update /usr/sbin/chromeos-update && echo "Patched update script installed!" || echo "Failed to installed patched update script."
+curl -l https://raw.githubusercontent.com/WesBosch/brunch-toolkit/main/chromeos-update -o /var/brunch/toolkit/chromeos-update || echo "Failed to download patched update script."
+sudo cp /var/brunch/toolkit/chromeos-update /usr/sbin/chromeos-update && echo "Patched update script installed!" || echo "Failed to copy patched update script."
 else
-echo "Your Brunch release may not need patched."
+echo "Your Brunch release is $current_brunch_version and may not need patched."
 fi 
+sudo rm /var/brunch/toolkit/chromeos-update || echo "Failed to clean up workspace."
